de62357a5bb1d0d4b2b2d832504db0ff5994afb4
Aecho <aecho@aethersi.com>
2017-03-13 10:11:56 +0800

    My hello world jersey example. First commit.

---
+++ src/testJavaWebService/Hello.java
@@ -0,0 +1,23 @@
+package testJavaWebService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello Jersey";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello()
+	{
+		return "<html><body><h1> Hello Jersey </h1></body></html>";
+	}
+}
